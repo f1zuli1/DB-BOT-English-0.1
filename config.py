68c7bdbe0d6fcd20c4af2e40bce067af3d7f3be3
Fuzuli <fuzuliisr06@gmail.com>
2025-11-07 22:19:19 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-token = ''
+import os
+
+token = os.getenv("token")
+
+if not token:
+    print("⚠️ Token tapılmadı! Railway Variables hissəsini yoxla.")
+else:
+    print("✅ Token uğurla tapıldı!")
+'
 DATABASE = 'db.db'
-
 DB_FILE = "photoandvideo.db"
-
-
-
-
-
